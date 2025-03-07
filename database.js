7ed2e612d67a5bb02274a605e0f20d80686d0798
ELEN FABIANA GRECCO <elen_grecco@hotmail.com>
2025-03-07 19:12:42 -0300

    Atualização

--- database.js
+++ database.js
@@ -3,27 +3,24 @@
  * Uso de framework mongoose
  */
 
-//importação do mongoose
+
 
 const mongoose = require('mongoose')
 
-//configuração do banco de dados
-//ip/link do servidor, autentificação, nome do banco
-//ao final da url definir um banco de dados
-//exemplo: /dbclientes
+
 const url = 'mongodb+srv://admin:123Senac@cluster0.qwga8.mongodb.net/dbnotes'
 
 
-//validação (evitar a abertura de várias conexões)
+
 let conectado = false
 
-//método ou função para conectar
+
 const conectar = async () => {
-    //Se não estiver conectado 
+    
     if(!conectado) {
         try {
-            await mongoose.connect(url) // conectar
-            conectado = true //setar a variavel
+            await mongoose.connect(url) 
+            conectado = true
             console.log("MongoDB Conectado")
         } catch (error) {
             console.error(error)
@@ -31,13 +28,13 @@ const conectar = async () => {
     }
 }
 
-//método ou função para desconectar
+
 const desconectar = async () => {
-    //se estiver conectado
+    
     if(conectado) {
-        //desconectar
+        
         try {
-            await mongoose.disconnect(url) //desconectar
+            await mongoose.disconnect(url) 
             conectado = false 
             console.log("MongoDB desconectado")
         } catch (error) {
@@ -46,5 +43,5 @@ const desconectar = async () => {
     }
 }
 
-//exportar para o main os métodos conectar e desconectar
+
 module.exports = {conectar, desconectar}
--- main.js
+++ main.js
@@ -1,23 +1,17 @@
 console.log("Electron - Processo principal")
 
-//importação dos recursos do framework 
-//app (se refere a aplicação)
-//BrowserWindow (criação da janela)
-//nativeTheme (definir tema claro ou escuro)
-//Menu (definir um menu personalizado)
-// shell (acessar links externo no navegador)
+
 const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
 
-//Janela principal
+
 let win
 const createWindow = () => {
-  // definindo o tema da janela claro ou escuro
+  
   nativeTheme.themeSource = 'light'
   win = new BrowserWindow({
-    //Para o sistema Sticky Notes o ideal para boas práticas a largura e altura este seria o ideal, devido a resolução (monitor padrão)
-    //width = largura
+    
     width: 1010,
-    //height = altura
+    
     height: 720,
     //frame: false,
     //resizable: false,
@@ -26,20 +20,19 @@ const createWindow = () => {
     //autoHideMenuBar: true
   })
 
-  //carregar o menu personalizado
-  //Atenção! Antes importar o recurso Menu
+  
   Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
-  //carregar o documento HTML na janela
+  
   win.loadFile('./src/views/index.html')
 }
 
-// janela Sobre
+
 function aboutWindow() {
   nativeTheme.themeSource='light'
-  // Obter a janela principal
+  
   const mainWindow = BrowserWindow.getFocusedWindow()
-  // Validação (se existir a janela principal)
+  
   if (mainWindow) {
     about = new BrowserWindow({
       width: 320,
@@ -47,10 +40,10 @@ function aboutWindow() {
       autoHideMenuBar: true,
       resizable: false,
       minimizable: false,
-      // Estabelecer uma relação hierárquica entre janelas
+      
       parent: mainWindow,
 
-      // criar uma janela modal (só retorna a principal quando encerrada)
+      
       modal: true
     })
   }
@@ -62,7 +55,7 @@ function aboutWindow() {
 app.whenReady().then(() => {
   createWindow()
 
-  //só ativar a janela principal se nenhuma outra estiver ativa
+  
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
@@ -70,25 +63,22 @@ app.whenReady().then(() => {
   })
 })
 
-//se o sistema não for MAC encerrar a aplicação quando a janela for fechada
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-// Reduzir a verbosidade de logs não críticos (devtools)
+
 app.commandLine.appendSwitch('log-level','3')
 
-//template do menu
+
 const template = [
   {
-    label: 'Notas',
+    label: 'Cadastro',
     submenu: [
-      {
-        label: 'Criar nota',
-        accelerator: 'Ctrl+N'  
-      },
+      
       {
         type: 'separator'
       },
@@ -96,6 +86,21 @@ const template = [
         label: 'Sair',
         accelerator: 'Alt+F4',
         click: () => app.quit()
+      }
+    ]
+  },
+
+  {
+    label: 'Relatório',
+    submenu: [
+      
+      {
+        label: 'Clientes',
+        
+      },
+
+      {
+        type: 'separator'
       }
     ]
   },
